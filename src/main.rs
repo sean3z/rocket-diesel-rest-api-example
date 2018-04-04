14b7e59369847550cfdf37db9636b422b34425b3
Ivan Tham <pickfire@riseup.net>
2018-04-05 01:00:27 +0800

    clean up dependencies and modules
    - use r2d2 feature in diesel for easier dependency management
    - simpify rocket_contrib dependency for same default features
    - add simple documentation and clean up r2d2 imports in db.rs

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@
 extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
-
 #[macro_use] extern crate diesel;
-extern crate r2d2;
-extern crate r2d2_diesel;
 
 use rocket_contrib::{Json, Value};
 
